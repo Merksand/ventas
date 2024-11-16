1d1f90e86bf0c48f97f2b3dee1e81177e22c4678
Merksand <miguelangelsp7161@gmail.com>
2024-11-16 18:59:35 -0400

    next

--- core/app/layouts/layout.php
+++ core/app/layouts/layout.php
@@ -45,7 +45,7 @@
           <!-- mini logo for sidebar mini 50x50 pixels -->
           <span class="logo-mini"><b>I</b>L</span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>Electro</b>Tech</span>
+          <span class="logo-lg"><b>Electrozzzzzzzzzz</b>Tech</span>
         </a>
 
         <!-- Header Navbar -->
